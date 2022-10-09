ab2f4d483832d9418b70adef07dca9c82614e1bd
unknown <godgodx4@gmail.com>
2022-10-09 16:02:25 +0900

    fix some for WiFiConnection

--- src/WifiConnection.h
+++ src/WifiConnection.h
@@ -1,22 +1,26 @@
+#ifndef WIFI_CONNECTION_H
+#define WIFI_CONNECTION_H
+
 #include <WiFiMulti.h>
 
 #define TRY_CONNECT_AP 50
 
 bool connectAP(const char *ssid, const char *pass){
+  WiFiMulti wifi;
   uint8_t cnt = 0;
 
-  WiFi.mode(WIFI_MODE_APSTA);
+  WiFi.mode(WIFI_MODE_STA);
   WiFi.disconnect(true);
+  WiFi.begin("0","0");
   delay(1000);
-  WiFiMulti wifi;
 
   wifi.addAP(ssid, pass);
-  while (wifi.run() != WL_CONNECTED && cnt < TRY_CONNECT_AP){
+  while((wifi.run(500) != WL_CONNECTED) && (cnt < TRY_CONNECT_AP)){
     delay(500);
     Serial.print(".");
     cnt += 1;
   }
-  if(wifi.run() != WL_CONNECTED) return false;
+  if(wifi.run(1) != WL_CONNECTED) return false;
 
   Serial.println("\nWiFi connected.");
   Serial.println("IP address: ");
@@ -26,7 +30,7 @@ bool connectAP(const char *ssid, const char *pass){
 }
 
 void startAP(const char *ssid, const char *pass){
-  WiFi.mode(WIFI_MODE_APSTA);
+  WiFi.mode(WIFI_MODE_AP);
   WiFi.softAP(ssid, pass);
 }
 
@@ -45,3 +49,5 @@ bool checkNetwork(String target){
 
   return result;
 }
+
+#endif
