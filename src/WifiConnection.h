5c9ecfcb69cecb708240539eec7baa066049fcec
unknown <godgodx4@gmail.com>
2024-05-02 14:21:15 +0900

    split WifiConnection header and implements

--- src/WifiConnection.h
+++ src/WifiConnection.h
@@ -3,65 +3,8 @@
 
 #include <WiFi.h>
 
-bool connectAP(const char *ssid, const char *pass, unsigned long connectWaitMillis = 10000, unsigned long disconnectWaitMillis = 1000, bool withSerialPrint = true){
-  unsigned long startTime;
-  bool connectionResult = true;
-
-  if(withSerialPrint) Serial.println("WiFi initializing");
-  WiFi.disconnect(true, true);
-  startTime = millis();
-  while(WiFi.status() == WL_CONNECTED){
-    if(withSerialPrint) Serial.print(".");
-
-    if((millis() - startTime) > disconnectWaitMillis) return false;
-
-    vTaskDelay(100);
-  }
-
-  if(withSerialPrint) Serial.println("WiFi connecting");
-  WiFi.mode(WIFI_MODE_STA);
-  WiFi.begin(ssid, pass);
-  startTime = millis();
-  while(WiFi.status() != WL_CONNECTED){
-    if(withSerialPrint) Serial.print(".");
-    
-    if((millis() - startTime) > connectWaitMillis){
-      connectionResult = false;
-      break;
-    }
-
-    vTaskDelay(100);
-  }
-  if(withSerialPrint) Serial.println("");
-
-  if(connectionResult && withSerialPrint){
-    Serial.println("WiFi connected.");
-    Serial.println("IP address: ");
-    Serial.println(WiFi.localIP());
-  }else WiFi.disconnect(true, true);
-
-  return connectionResult;
-}
-
-void startAP(const char *ssid, const char *pass){
-  WiFi.mode(WIFI_MODE_AP);
-  WiFi.softAP(ssid, pass);
-}
-
-bool checkNetwork(String target){
-  bool result = false;
-  uint16_t nets = WiFi.scanNetworks();
-
-  for(int i = 0; i < nets; i++){
-    String ssid = WiFi.SSID(i);
-
-    if(ssid == target){
-      result = true;
-      break;
-    }
-  }
-
-  return result;
-}
+bool connectAP(const char *ssid, const char *pass, unsigned long connectWaitMillis = 10000, unsigned long disconnectWaitMillis = 1000, bool withSerialPrint = true);
+void startAP(const char *ssid, const char *pass);
+bool checkNetwork(String target);
 
 #endif
